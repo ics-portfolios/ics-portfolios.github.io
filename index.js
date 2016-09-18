a4e5da999343e443dc8c37b35fd60393280cec99
Philip Johnson <johnson@hawaii.edu>
2016-09-18 08:07:41 -1000

    continue fiddling

--- index.js
+++ index.js
@@ -1,25 +1,54 @@
 #! /usr/bin/env node
 
+// 1. Create a profile-data.json file.
+//    [{ domain: 'philipmjohnson.github.io', techfolio: true, bio-overrides: { name: 'Philip Johnson' }}]
+// 2. Read the profiles.json file in as text, convert to JSON, store as profileData.
+// 3. Add a default field called bio with name, homepage, picture, interests, summary fields.
+// 2  If techfolio, update bio field based on bio.json file data.
+// 4. Override bio fields with bio-overrides.
+// 3. Write out a data file to this _data directory with info for Jekyll.
+
 console.log('Starting update_ics_portfolios');
 const request = require('request');
 
-const urls = ['http://www.google.com', 'http://philipmjohnson.org'];
-const urlBodies = [];
+// URL: https://raw.githubusercontent.com/philipmjohnson/philipmjohnson.github.io/master/_data/bio.json
 
-const urlBodyPromises = urls.map(function (url) {
+function getBioJsonUrl(domain) {
+  const username = domain.split('.')[0];
+  return `https://raw.githubusercontent.com/${username}/${domain}/master/_data/bio.json`;
+}
+
+const domains = ['philipmjohnson.github.io'];
+
+const bioJsons = [];
+
+/**
+ * Returns a Promise which, when resolved, results in pushing the bio.json onto bioJsons.
+ * If an error occurs, prints out a message to the console with the problematic URL.
+ * @param url The URL to get the body of.
+ * @returns {Promise} A Promise to push that body onto bioJsons.
+ */
+function getBioFiles(domain) {
   return new Promise(function (resolve) {
-    request.get(url, function processUrl(error, response, body) {
+    request.get(getBioJsonUrl(domain), function processUrl(error, response, body) {
       if (!error && response.statusCode === 200) {
-        urlBodies.push(body);
+        try {
+          bioJsons.push(JSON.parse(body));
+        } catch (e) {
+          console.log(`Failed to parse bio.json for ${domain}.`);
+        }
       } else {
-        console.log(`Failed to get body of ${url}.`);
+        console.log(`Failed to get bio.json for ${domain}.`);
       }
       resolve();
     });
   });
-});
+}
 
+const urlBodyPromises = domains.map(getBioFiles);
 
 Promise.all(urlBodyPromises)
-    .then(function () { console.log(`finished, got ${urlBodies.length} bodies`); })
+    .then(function () {
+      console.log(bioJsons);
+    })
     .catch(console.error);
