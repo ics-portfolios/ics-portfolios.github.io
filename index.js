894324d6fa9c0903734477aba0052392f73a56f8
Philip Johnson <johnson@hawaii.edu>
2016-09-19 08:49:11 -1000

    updates of the day

--- index.js
+++ index.js
@@ -1,31 +1,43 @@
 #! /usr/bin/env node
 
-// 1. Create a profile-data.json file.
-//    [{ domain: 'philipmjohnson.github.io', techfolio: true, bio-overrides: { name: 'Philip Johnson' }}]
-// 2. Read the profiles.json file in as text, convert to JSON, store as profileData.
-// 3. Add a default field called bio with name, homepage, picture, interests, summary fields.
-// 2  If techfolio, update bio field based on bio.json file data.
-// 4. Override bio fields with bio-overrides.
-// 3. Write out a data file to this _data directory with info for Jekyll.
+// 1. Merge the bio.json file into the profile data. Match the techfolio key against the website field.
+//    This will fail for me, unfortunately, unless I update my bio.json.
+//    I should do that for now, then when I implement defaults, I can override somehow.
+//    The website is the only unique key I can use to match profile entries to the bio.json file.
+// 3. Write out a data file to the _data directory with appropriate info for Jekyll.
+// 4. Start on HTML.
 // 10. Image processing: https://github.com/EyalAr/lwip
 
 console.log('Starting update_ics_portfolios');
 const request = require('request');
+const fs = require('fs');
+const _ = require('underscore');
+
+/** Location of the profile entries file. */
+const profileEntriesFile = 'profile-entries.json';
+
+/** Holds the profile data, initialized with profile-entries, then updated with bio.json. */
+const profileData = [];
 
 /**
- * Given a github.io domain, returns the URL to the bio.json file.
- * @param domain A domain, such as 'philipmjohnson.github.io'.
- * @returns {string} The corresponding URL to the bio.json file.
+ * Initializes profileData with the contents of the profile-entries file.
  */
-function getBioJsonUrl(domain) {
-  // URL: https://raw.githubusercontent.com/philipmjohnson/philipmjohnson.github.io/master/_data/bio.json
-  const username = domain.split('.')[0];
-  return `https://raw.githubusercontent.com/${username}/${domain}/master/_data/bio.json`;
+function initializeProfileData() {
+  const contents = fs.readFileSync(profileEntriesFile, 'utf8');
+  const data = JSON.parse(contents);
+  _.each(data, function (entry) { profileData.push(entry); });
 }
 
-const domains = ['philipmjohnson.github.io'];
-
-const bioJsons = [];
+/**
+ * Given a techfolio hostname, returns the URL to its associated bio.json file.
+ * @param techfolioHostName A domain, such as 'philipmjohnson.github.io'.
+ * @returns {string} The corresponding URL to the bio.json file.
+ */
+function getBioJsonUrl(techfolioHostName) {
+  // URL: https://raw.githubusercontent.com/philipmjohnson/philipmjohnson.github.io/master/_data/bio.json
+  const username = techfolioHostName.split('.')[0];
+  return `https://raw.githubusercontent.com/${username}/${techfolioHostName}/master/_data/bio.json`;
+}
 
 /**
  * Returns a Promise which, when resolved, results in pushing the bio.json onto bioJsons.
@@ -34,26 +46,33 @@ const bioJsons = [];
  * @returns {Promise} A Promise to push that body onto bioJsons.
  */
 function getBioFiles(domain) {
-  return new Promise(function (resolve) {
+  return new Promise(function (resolve, reject) {
     request.get(getBioJsonUrl(domain), function processUrl(error, response, body) {
       if (!error && response.statusCode === 200) {
         try {
-          bioJsons.push(JSON.parse(body));
+          resolve(JSON.parse(body));
         } catch (e) {
-          console.log(`Failed to parse bio.json for ${domain}.`);
+          reject(new Error(`Failed to parse bio.json for ${domain}.`));
         }
       } else {
-        console.log(`Failed to get bio.json for ${domain}.`);
+        reject(new Error(`Failed to get bio.json for ${domain}.`));
       }
-      resolve();
     });
   });
 }
 
-const urlBodyPromises = domains.map(getBioFiles);
+// ////////////////////  Start the script. ////////////////////////////////////////////
 
+// Set profileData to the contents of the profile entries.
+initializeProfileData();
+
+// Create a set of promises for reading in the bio.json files associated with every entry.
+// Note that profile-entries cannot yet handle non-Techfolio data.
+const urlBodyPromises = _.map(profileData, function (entry) { return getBioFiles(entry.techfolio); });
+
+// Retrieve the bio.json files.
 Promise.all(urlBodyPromises)
-    .then(function () {
-      console.log(bioJsons);
+    .then(function (body) {
+      console.log(body);
     })
     .catch(console.error);
