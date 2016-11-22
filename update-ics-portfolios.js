3c5cd5de22eb5427a5a6c5fc067d5c2e51c405bc
man chon <mckuok@hawaii.edu>
2016-11-21 21:13:55 -1000

    Modularize interest filter

--- update-ics-portfolios.js
+++ update-ics-portfolios.js
@@ -8,26 +8,15 @@ const fs = require('fs');
 const _ = require('underscore');
 const jsonfile = require('jsonfile');
 const jsonic = require('jsonic');
+const filterInterest = require('./interest-filter');
 
 const dataFile = '_data/data.json';
-const synonymFile = '_data/synonyms.json'
-const interestFile = '_data/interests.json';
-const unclassifiedInterestsFile = '_data/unclassifiedInterests.json';
 
 /** Location of the profile entries file. */
 const profileEntriesFile = 'profile-entries.json';
 
 /** Holds the profile data, initialized with profile-entries, then updated with bio.json. */
 const profileData = [];
-
-/** Holds all the term to synonyms data, defined in synonymFile **/
-const synonymData = [];
-
-/** Holds all the interest data for each person **/
-const interestData = [];
-
-/** Holds all the names that have interests unclassified **/
-const unclassifiedInterests = [];
 
 /**
  * Initializes profileData with the contents of the profile-entries file, and load synonyms from synonyms.json.
@@ -39,12 +28,7 @@ function initializeData() {
     profileData.push(entry);
   });
 
-  const synonymContents = fs.readFileSync(synonymFile, 'utf8');
-  data = jsonic(synonymContents);
-  _.each(data, function(interest) {
-    synonymData.push(interest);
-  })
-
+  filterInterest.initData();
 }
 
 /**
@@ -129,7 +113,7 @@ function updateProfileEntry(bio) {
       var level = profileEntry.level;
 
       for(var i = 0; i < profileEntry.interests.length; i++) {
-        groupInterest(profileEntry.interests[i], username, level);
+        filterInterest.groupInterest(profileEntry.interests[i], username, level);
       }
     } else {
       console.log(`Could not find profile entry corresponding to ${bioHostName} (${bio.basics.name})`);
@@ -154,29 +138,7 @@ function writeJekyllInfoFiles() {
     console.error(err);
   });
 
-  /*for (var level in interestData) {
-    var levelData = _.sortBy(interestData[level], function(interestGroup) {
-      return interestGroup.names.length * -1;  
-    });
-
-    interestData[level] = levelData;
-  }*/
-
-
-  jsonfile.writeFile(interestFile, interestData, function (err) {
-    console.error(err);
-  });
-
-  jsonfile.writeFile(unclassifiedInterestsFile, unclassifiedInterests, function(err) {
-    console.error(err);
-  });
-
-  if (unclassifiedInterests.length > 0) {
-    console.log("Check " + unclassifiedInterestsFile + " for unclassified interests");
-  } else {
-    console.log("All interests are mapped to a term");  
-  }
-
+  filterInterest.writeToFile();
 }
 
 function updateProfileDataFromLocalBio(localProfiles) {
@@ -186,85 +148,6 @@ function updateProfileDataFromLocalBio(localProfiles) {
     const bio = jsonic(contents);
     updateProfileEntry(bio);
   });
-}
-
-/* Group interest together to the corresponding interest in interestData */
-function groupInterest(interest, name, level) {
-  var term = "unclassified";
-  var foundInSynonyms = false;
-
-  for (var i = 0; i < synonymData.length; i++) {
-    var matched = _.find(synonymData[i].synonyms, function(synonym) {  // loop through all synoyms for a given term 
-      return compareInterestAndSynonym(interest, synonym);
-    });
-
-    if (matched != null) {            // a term for the interest is found
-      term = synonymData[i].term;
-      foundInSynonyms = true;
-      break;
-    } 
-  }
-
-  if (foundInSynonyms) {
-    var levelIndex = findLevelIndex(level);
-
-    if (levelIndex >= 0) {
-      for (var i = 0; i < interestData[levelIndex].data.length; i++) {
-        if (interestData[levelIndex].data[i].interest.toLowerCase() === term.toLowerCase()) {   // insert the interest into interestData
-          interestData[levelIndex].data[i].names.push(name);
-          return;
-        }
-      }
-    }
-
-    createInterestGroup(term, name, levelIndex, level);    // if this interest is not found in interestData, start its own group
-
-  } else {
-    insertToUnclassifiedInterests(interest);    // not found, push to unclassified Interest 
-  } 
-}
-
-/* Create a new interest group using the given interest and name */
-function createInterestGroup(interest, name, levelIndex, level) {
-  const interestGroup = {
-    interest: interest,
-    names: [name]  
-  }
-  
-  if (levelIndex >= 0) {
-    interestData[levelIndex].data.push(interestGroup);
-  } else {
-    const levelGroup = {
-      level: level,
-      data: [interestGroup]
-    }
-    interestData.push(levelGroup);
-  }
-}
-
-function findLevelIndex(level)  {
-  for (var i = 0; i < interestData.length; i++) {
-    if (interestData[i].level.toLowerCase() === level.toLowerCase()) {
-      return i;
-    }
-  }
-  return -1;
-}
-
-/* Insert interest to unclassifiedInterests */
-function insertToUnclassifiedInterests(interest) {
-  unclassifiedInterests.push(interest);
-}
-
-/* Compare the given interest and the synonym to see if they refer to the same term */
-function compareInterestAndSynonym(interest, synonym) {
-  interest = interest.toLowerCase().replace(/ /g, "");
-  synonym = synonym.toLowerCase().replace(/ /g, "");
-  if (synonym.length <= 3) {       // handle acronyms
-    return interest === synonym;
-  } else {
-    return interest.indexOf(synonym) != -1;
-  }
 }
 
 // ////////////////////  Start the script. ////////////////////////////////////////////
