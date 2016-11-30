4e0bac7e933c383aad4d705f84f34ae073cf85c4
man chon <mckuok@hawaii.edu>
2016-11-29 23:09:06 -1000

    Hall of fame redesign

--- hall-of-fame-html-generator.js
+++ hall-of-fame-html-generator.js
@@ -7,7 +7,6 @@ const request = require('request');
 const fs = require('fs');
 const _ = require('underscore');
 const jsonic = require('jsonic');
-const cheerio = require('cheerio');
 const async = require('async');
 
 /* Files */
@@ -16,47 +15,50 @@ const profileTemplateFile = '_includes/portfolio-card.html'
 const hallOfFameCardsFile = '_includes/hallOfFameCards.html';
 
 /* constant */
-const TYPE_ESSAY = 'Exceptional Essays';
-const TYPE_PROJECT = 'Exceptional Projects';
-const TYPE_PROFILE = 'Exceptional Profiles';
+const TYPE_ESSAY = 'essay';
+const TYPE_PROJECT = 'project';
+const TYPE_PROFILE = 'site';
 
-/* placeholder variable to cache template */
-var profileTemplate;
-var storedProfileData;
 
 /* Generate template codes for hall of fame files*/
 function generateHallOfFameTemplate(profileData) {  
   const hallOfFameContents = fs.readFileSync(hallOfFameURLFile, 'utf8');
-  var urls = jsonic(hallOfFameContents);
-  storedProfileData = profileData;
-
-  if (urls.length > 0) {
+  var works = jsonic(hallOfFameContents);
+  
+  if (works.length > 0) {
     var tasks = [];
     var essays = [];
     var projects = [];
     var profiles = [];
 
-    for (var i = 0; i < urls.length; i++) {
-      if (urls[i].indexOf('/essays/') >= 0) {
-        essays.push(urls[i]);
-      }
-      else if (urls[i].indexOf('/projects/') >= 0) {
-        projects.push(urls[i]);
-      } 
-      else if (urls[i].replace('https://', '').replace('http://', '').split('.').length === 3) {
-        profiles.push(urls[i]);
+    for (var i = 0; i < works.length; i++) {
+
+      var work = attachIdentificationFields(works[i], profileData);
+
+      if (work != null) {
+        if (work.type === TYPE_ESSAY) {
+          essays.push(work);
+        }
+        else if (work.type === TYPE_PROJECT) {
+          projects.push(work);
+        } 
+        else if (work.type === TYPE_PROFILE) {
+          profiles.push(work);
+        }
+      } else {
+        console.log("Cannot find the author in data.json for " + works[i].title + " using the given github username");
       }
     }
 
     tasks.push(
       function(callback) {
-        addExceptionalEssays(essays, "", callback);
+        addExceptionalWorkHTML("Sites", profiles, "", callback);
       },
       function(html, callback) {
-        addExceptionalProjects(projects, html, callback);
+        addExceptionalWorkHTML("Projects", projects, html, callback);
       },
       function(html, callback) {
-        addExceptionalProfiles(profiles, html, callback);
+        addExceptionalWorkHTML("Essays", essays, html, callback);
       }
     );
 
@@ -74,100 +76,58 @@ function generateHallOfFameTemplate(profileData) {
   } 
 }
 
-/* Get profile card HTML */
-function getProfileHTML(url, html, callback) {
-  console.log("Reading " + url + " as a profile card for hall of fame");
-  var profile = getProfileData(url);
-  if (profile != null) {
-    if (profileTemplate == null || profileTemplate.length === 0) {
-      profileTemplate = fs.readFileSync(profileTemplateFile, 'utf8').replace(/{%.*%}/g, '');
-    }
-    var interests = "";
-    _.each(profile.interests, function(interest) {
-      interests += interest + ', ';
-    });
-    interests = interests.substring(0, interests.length - 2);
-
-    var profileHTML = profileTemplate.format(profile.username, profile.picture, profile.name, profile.label, profile.summary, interests, profile.website)
-                        .replace(/href/g, 'target="_blank" href');
-
-    callback(null, html + profileHTML + '\n');
-  } else {
-    console.log("Unable to get data for " + url + ", perhaps the person's data is not in our database");
-    callback(null, html + '\n');
-  }
-}
-
-/* Get profile data based on the url for that profile's homepage */
-function getProfileData(url) {
-  var matchingProfile = _.find(storedProfileData, function(profile) {
-    return profile.website.replace(/\//g, '') === url.replace(/\//g, '');
-  });
-
-  return matchingProfile; 
-}
-
-
 /* Get the card's HTML */
-function getExceptionalWorkHTML(url, html, callback) {
-  console.log("Reading " + url + " as a essay or project for hall of fame");
-  var summaryPageURL = url.substring(url.substring(0, url.length - 1), url.lastIndexOf("/") + 1);
-  var documentName = url.substring(summaryPageURL.length);
-  var baseURL = summaryPageURL.substring(0, summaryPageURL.length - 1);
-  baseURL = baseURL.substring(0, baseURL.lastIndexOf("/"));
-  
-  request(summaryPageURL, function (error, response, body) {
-    'use strict';
-    if (!error && response.statusCode == 200) {
-      let $ = cheerio.load(body);
-      $(".card").each(function(index, card) {
-        var cardHTML = $.html($(this));
-        if (cardHTML.indexOf(documentName) >= 0) {
-          var cardObject = replaceLinksToAbsolutePath($, cardHTML, baseURL);
-          callback(null, html + cardObject.html() + '\n');
-        }
-      });
-    }
-  });
+function addCardHTML(data, html, callback) {
+  const template =
+  `        
+  <div class="ui centered fluid card">
+    <div class="content">
+      <h5 class="header">{{ title }}}</h5>
+      <h3 class="ui header">
+        <img class="ui avatar image left floated" src="{{ img_url }}}">
+        {{ author }}
+      </h3>
+      <h4 class="ui sub header"> Selected Reason: </h4>
+      <div class="ui description"> 
+        <p> {{ reason }} </p>
+      </div>
+      <div class="meta">
+        <span class="right floated time">{{ date }}</span>
+      </div>
+    </div>
+    <div class="ui bottom attached button">
+      <a href={{ url }}> <i class="external icon"></i> View Work </a>
+    </div>
+  </div>
+  `;
+  var entry = template.format(data.title, data.imgURL, data.author, data.reason, data.date, data.url);
+  callback(null, html + entry + "\n");
 }
 
 /* Add section header to the hall of fame section */
 function addSectionHeader(header, html, callback) {
   var headerTemplate =  
-    `<div class="ui raised segment">
-      <h1 class="ui header"> 
-        <i class="trophy icon"></i> {{ header }}} 
-      </h1>
-      <div class="ui divider"></div>
-      <div class="ui four centered stackable cards">`;
+    ` 
+    <div class="column">
+      
+      <h3 class="ui centered header"> 
+        <img src="img/trophy.png"></i> 
+        {{ header }}
+      </h3> 
+    `;
 
   callback(null, html + headerTemplate.format(header) + '\n');
 }
 
 /* Add section closing divs to the hall of fame section */
 function addSectionClosingDivs(html, callback) {
-  callback(null, html + '</div></div>' + '\n');
+  callback(null, html + '</div>' + '\n');
 }
 
-/* Add exceptional essays from the URLs */
-function addExceptionalEssays(urls, startHtml, callback) {
-  addSection(urls, startHtml, TYPE_ESSAY, getExceptionalWorkHTML, callback);
-}
-
-/* Add exceptional projects from the URLs */
-function addExceptionalProjects(urls, startHtml, callback) {
-  addSection(urls, startHtml, TYPE_PROJECT, getExceptionalWorkHTML, callback);
-}
-
-/* Add exceptional profiles from the URLs */
-function addExceptionalProfiles(urls, startHtml, callback) {
-  addSection(urls, startHtml, TYPE_PROFILE, getProfileHTML, callback);
-}
-
-/* Add a section appened to the startHtml */
-function addSection(urls, startHtml, type, generator, callback) {
-  if (urls.length > 0) {
-    var iterator = makeIterator(urls);
+/* Append exceptional works to startHtlm */
+function addExceptionalWorkHTML(type, works, startHtml, callback) {
+  if (works.length > 0) {
+    var iterator = makeIterator(works);
     var tasks = [];
     
     tasks.push(
@@ -176,10 +136,10 @@ function addSection(urls, startHtml, type, generator, callback) {
       }
     );
     
-    for (var i = 0; i < urls.length ; i++) { 
+    for (var i = 0; i < works.length ; i++) { 
       tasks.push(
         function(html, callback1) {
-          generator(iterator.next().value, html, callback1);
+          addCardHTML(iterator.next().value, html, callback1);
         }
       );
     }
@@ -198,35 +158,18 @@ function addSection(urls, startHtml, type, generator, callback) {
   }
 }
 
-/* Replace card's href and src to absolute paths*/
-function replaceLinksToAbsolutePath($, cardHTML, baseURL) {
-  var cardObject = $('<div>' + cardHTML + '</div>');
-  cardObject.find('a').each(function() {
-    var href = $(this).attr('href');
-    $(this).attr('href', toAbsoluteURL(href, baseURL));
-    $(this).attr('target', '_blank');
+/* Attach extra fields to the work by looping through profileData to find the author's name and avatar url*/
+function attachIdentificationFields(work, profileData) {
+  var matchingProfile = _.find(profileData, function(profile) {
+    return profile.username.toLowerCase() === work.github.toLowerCase()
   });
 
-  cardObject.find('img').each(function() {
-    var href = $(this).attr('src');
-    $(this).attr('src', toAbsoluteURL(href, baseURL));
-  });  
-
-  return cardObject;
-}
-
-/* convert relative url to absolute url */
-function toAbsoluteURL(relativeURL, base) {
-  if (relativeURL.indexOf("http") < 0) {
-    if (!base.endsWith("/")) {
-      base += "/";
-    }
-    if (relativeURL.startsWith("/")) {
-      relativeURL = relativeURL.substring(1);
-    }
-    return base + relativeURL;
+  if (matchingProfile != null) {
+    work.author = matchingProfile.name;
+    work.imgURL = matchingProfile.picture;
+    return work;
   } else {
-    return relativeURL;
+    return null;
   }
 }
 
@@ -238,7 +181,6 @@ String.prototype.format = function() {
     }
     return formatted;
 };
-
 
 /* Util method for making an iterator for array */
 function makeIterator(array){
